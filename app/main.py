9ed7ba44d671a1bacca58a30817fb9c1f69020c3
Manuel Arango <juan12292004@outlook.com>
2025-12-02 20:58:43 -0500

    add: add autentication

--- app/main.py
+++ app/main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
-
 from app.routes import auth_routes, context_routes, user_routes
+from app.services.email.resend_email_service import ResendEmailService
+import os
 
 app = FastAPI(title="Voice Auth App")
+
+email_service = ResendEmailService(
+    api_key=os.getenv("RESEND_API_KEY"),
+    sender=os.getenv("EMAIL_FROM")
+)
+
+#Guardar en app.state para acceder desde las rutas
+app.state.email_service = email_service
 
 app.include_router(user_routes.router, tags=["Users"])
 app.include_router(auth_routes.router, tags=["Auth"])
